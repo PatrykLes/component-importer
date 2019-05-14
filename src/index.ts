c2b98690dca45e9a1d101ff806b545d95c3d2d8d
Dan-el Khen <danelkhen@gmail.com>
2019-05-14 16:40:42 +0200

    Fix imported Props name

--- src/index.ts
+++ src/index.ts
@@ -105,7 +105,7 @@ import * as React from "react"
 import * as System from "../../design-system"
 import { ControlType, PropertyControls } from "framer"
 
-type Props = ${componentName} & {
+type Props = ${componentName}Props & {
   width: number
   height: number
 }
