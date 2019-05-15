be1f6ae0261172401136182db28c55855f3941cf
Dan-el Khen <danelkhen@gmail.com>
2019-05-15 13:27:19 +0200

    change cli usage to use directory, preserve directory structure in output folder

--- src/cli.ts
+++ src/cli.ts
@@ -7,30 +7,34 @@ async function main() {
     if (!process.argv[2]) {
         console.log("")
         console.log("Usage:")
-        console.log("yarn cli [file-pattern] [out-dir]")
+        console.log("yarn cli [src-dir] [out-dir]")
         console.log("")
         console.log("Example:")
-        console.log("yarn cli ../my-project/src/**/*.tsx ../my-project/framer")
+        console.log("yarn cli ../my-project/src ../my-project/framer")
         console.log("")
         return
     }
-    const pattern = process.argv[2]
+    const srcDir = process.argv[2]
     const outDir = process.argv[3]
+    const pattern = "**/*.tsx"
     console.log({ pattern, outDir })
-    const files = await new Promise<string[]>(resolve => glob(pattern, (err, files) => resolve(files)))
-    console.log(files)
-    for (const file of files) {
-        console.log("Processing", file)
-        const code = await processFile(file)
+    const relativeFiles = await new Promise<string[]>(resolve =>
+        glob(pattern, { cwd: srcDir }, (err, files) => resolve(files)),
+    )
+    console.log(relativeFiles)
+    for (const relativeFile of relativeFiles) {
+        console.log("Processing", relativeFile)
+        const srcFile = path.join(srcDir, relativeFile)
+        const code = await processFile(srcFile)
         if (!code) {
-            console.log("Skipping", file)
+            console.log("Skipping", relativeFile)
             continue
         }
         if (!outDir) {
             console.log(code)
             continue
         }
-        const outFile = path.join(outDir, path.basename(file))
+        const outFile = path.join(outDir, relativeFile)
         console.log("Saving", outFile)
         await fse.ensureDir(path.dirname(outFile))
         await fse.writeFile(outFile, code)
