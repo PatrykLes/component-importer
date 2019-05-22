f94b194017e9fc1093da90c78f83cf142c47995f
Dan-el Khen <danelkhen@gmail.com>
2019-05-22 10:14:45 +0200

    basic support to props type detection and interface props

--- src/process.ts
+++ src/process.ts
@@ -12,8 +12,9 @@ export async function processFile(file: string): Promise<string> {
 
 function analyze(file: string, contents: string): AnalyzedFile {
     const sourceFile = ts.createSourceFile(file, contents, ts.ScriptTarget.ESNext, true, ts.ScriptKind.TSX)
-    const name = findComponentName(sourceFile)
-    const propsType = findPropsType(sourceFile)
+    const comp = findComponentNameAndType(sourceFile)
+    const propsTypeName = getFirstGenericArgument(comp.type).getText()
+    const propsType = findPropsType(sourceFile, propsTypeName)
     const res: AnalyzedFile = {
         file,
         componentName: null,
@@ -21,12 +22,12 @@ function analyze(file: string, contents: string): AnalyzedFile {
         propertyControls: new PropertyControls(),
     }
 
-    if (!name) console.log("Can't find component in file")
-    if (!propsType) console.log("Can't find props in file")
-    if (!name || !propsType) return res
+    if (!comp) console.warn("Can't find component in file")
+    if (!propsType) console.warn("Can't find props in file")
+    if (!comp || !propsType) return res
 
-    res.componentName = `System.${name}`
-    res.framerName = name
+    res.componentName = `System.${comp.name}`
+    res.framerName = comp.name
 
     for (const prop of propsType.members.filter(ts.isPropertySignature)) {
         let pc = new PropertyControl({ name: prop.name.getText() })
@@ -83,17 +84,39 @@ export class ${framerName} extends React.Component<Props> {
 `
 }
 
-function findPropsType(sourceFile: ts.SourceFile): ts.TypeLiteralNode {
-    const node = sourceFile.statements.find(ts.isTypeAliasDeclaration)
-    if (!node) return null
-    const type = node.type
-    if (!ts.isTypeLiteralNode(type)) return null
-    return type
+function findPropsType(sourceFile: ts.SourceFile, name: string): ts.InterfaceDeclaration | ts.TypeLiteralNode {
+    const nodes = sourceFile.statements
+        .map(t => (ts.isTypeAliasDeclaration(t) || ts.isInterfaceDeclaration(t) ? t : null))
+        .filter(t => t != null)
+    for (const node of nodes) {
+        if (node.name.getText() != name) continue
+        if (ts.isTypeAliasDeclaration(node)) {
+            const type = node.type
+            if (ts.isTypeLiteralNode(type)) {
+                return type
+            }
+        }
+        if (ts.isInterfaceDeclaration(node)) {
+            return node
+        }
+    }
+    return null
 }
-function findComponentName(sourceFile: ts.SourceFile): string {
+function findComponentNameAndType(sourceFile: ts.SourceFile): NameAndType {
     const node = sourceFile.statements.find(ts.isVariableStatement)
     if (!node) return null
-    return node.declarationList.declarations[0].name.getText()
+    const decl = node.declarationList.declarations[0]
+    const name = decl.name.getText()
+    const type = decl.type
+    return { name, type }
+}
+
+function getFirstGenericArgument(type: ts.TypeNode): ts.TypeNode {
+    if (ts.isTypeReferenceNode(type)) {
+        const genericArgType = type.typeArguments[0]
+        return genericArgType
+    }
+    return null
 }
 
 class PropertyControls {
@@ -137,4 +160,9 @@ interface AnalyzedFile {
     componentName: string
     framerName: string
     propertyControls: PropertyControls
+}
+
+interface NameAndType {
+    name: string
+    type: ts.TypeNode
 }
