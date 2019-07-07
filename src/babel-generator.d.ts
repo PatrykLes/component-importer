f6e1a6896d6384173393463795b7c6936e2f94ad
Fernando Hurtado <fernandohur@gmail.com>
2019-07-07 17:02:41 +0200

    feat: Added design-systems to gitignore

--- src/babel-generator.d.ts
+++ src/babel-generator.d.ts
@@ -1,6 +1,10 @@
+import { GeneratorOptions } from "@babel/core"
+
 declare module "@babel/generator" {
     export default function(ast: any): GenerateResult
     export interface GenerateResult {
         code: string
     }
+
+    export interface GeneratorOptions {}
 }
