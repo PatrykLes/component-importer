ed755f997bd89e216bd7d7315e7a2f4082fade78
Fernando Hurtado <fernandohur@gmail.com>
2019-07-26 15:12:58 +0200

    feat: added additional properties to PropType

--- src/types.ts
+++ src/types.ts
@@ -46,24 +46,43 @@ export interface ComponentInfo {
 
 export type PropType =
     | {
-          type: "boolean" | "string"
+          type: "boolean"
           name: string
+          defaultValue?: boolean
+          title?: string
+      }
+    | {
+          type: "string"
+          name: string
+          defaultValue?: string
+          title?: string
+      }
+    | {
+          type: "color"
+          name: string
+          defaultValue?: string
+          title?: string
       }
     | {
           type: "number"
           name: string
           min?: number
           max?: number
+          step?: number
+          defaultValue?: number
+          title?: string
       }
     | {
           type: "enum"
           name: string
           possibleValues: any[]
+          title?: string
       }
     | {
           type: "array"
           name: string
           of: PropType
+          title?: string
       }
     | {
           type: "unsupported"
