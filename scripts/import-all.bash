7408b21e033d84497da3fe8bc62863ab23a8da98
Fernando Hurtado <fernandohur@gmail.com>
2019-09-11 16:37:36 +0200

    fix: run import-all before running tests

--- scripts/import-all.bash
+++ scripts/import-all.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
 
 #
 # Creates a new empty NPM package with the given dependency added.
@@ -22,16 +22,19 @@ set -euxo pipefail
 #       └─ ant-design/   <==== will add ant-design as a dependency on the given version
 #
 function import-design-system() {
+    DEP_NAME="$1"
+    DIR="integration-test-data/$1"
+    VERSION="$2"
 
-	DEP_NAME="$1"
-	DIR="integration-test-data/$1"
-	VERSION="$2"
+    if [ -d "$DIR" ]; then
+        echo "Skipping importing $DEP_NAME because it already exists."
+        return
+    fi
 
-	rm -rf "$DIR"
-	mkdir -p "$DIR"
+    mkdir -p "$DIR"
 
-	yarn --cwd "$DIR" init --yes
-	yarn --cwd "$DIR" add "$DEP_NAME@$VERSION"
+    yarn --cwd "$DIR" init --yes
+    yarn --cwd "$DIR" add "$DEP_NAME@$VERSION"
 }
 
 import-design-system "antd" "3.22.2"
@@ -39,5 +42,6 @@ import-design-system "@blueprintjs/core" "3.18.0"
 import-design-system "grommet" "2.7.7"
 import-design-system "@material-ui/core" "4.4.0"
 # TODO: re-enable once module support has been added
+# See
 # import-design-system "baseui" "8.17.1"
 import-design-system "office-ui-fabric-react" "7.29.0"
